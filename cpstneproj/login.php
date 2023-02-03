78a143369ee68ab4524a26b2dc0f2ed179aa6355
John Paul Arlos <32674233+GLORIFIER7@users.noreply.github.com>
2023-02-03 23:05:37 +0800

    Login Update

--- cpstneproj/login.php
+++ cpstneproj/login.php
@@ -1,32 +1,15 @@
 <?php
-require_once("session.php");
-require_once("connections/conn.php");
-$con = connection();
+require_once('connections/inventoryclass.php');
 
-if(isset($_POST['login'])){
-    $email = $_POST['email'];
-    $password = md5($_POST['password']);
-
-   $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
-   $user = $con->query($sql) or die ($con->error);
-   $row = $user->fetch_assoc();
-   $total = $user->num_rows;
-
-        if($total > 0){
-            $_SESSION['UserLogin'] = $row['email'];
-            $_SESSION['Access'] = $row['user_lvl_id'];
-            echo header("Location: dashboard.php");
-        }else{
-            echo "No user found.";
-        }
-}
+$invt->login();
 
 ?>
 
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
-<head>
-<meta charset="UTF-8">
+  <head>
+  <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> -->
@@ -34,23 +17,47 @@ if(isset($_POST['login'])){
     <link rel="stylesheet" href="./components/css/datatables.bootstrap5.min.css" />
     <link rel="stylesheet" href="./components/css/style.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-</head>
-<body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+              div {
+            background-color: aquamarine;
+            border: 2px coral;
+        }
+        body.background{
+            filter: saturate(250%);
+            image-resolution: saturate(250);
+        }
+        
+    </style>
+
+    <title>Document</title>
+  </head>
+  <body background="https://jooinn.com/images/abstract-colorful-background-3.jpg" class="container-fluid">
+
+  
+  <div class="d-flex aligns-items-center justify-content-center card text-center position-absolute top-50 start-50 translate-middle" style="width: 20rem;">
+  
+  <div class="card-body">
+    <fieldset><legend><h3>Antropaul Inventory Corporation</h3></legend>
+    <h5 class="card-title">Login</h5>
+    <form method="post">
+  <div class="mb-3">
+    <label for="username" class="form-label">Username</label>
+    <input type="text" class="form-control" id="user_name" name="user_name" aria-describedby="emailHelp" placeholder="username here">
     
-<main class="mt-5 pt-5">
-        <div class="container-fluid">
-                <h1>Login Page</h1>
-                <br/>
-                <form action="" method="post">
-                <label>Email</label>
-                <input type="text" name="email" id="email">
-                <label>Password</label>
-                <input type="password" name="password" id="password">
-                <button type="submit" name="login">login</button>
-                </form>
-        </div>
-</main>
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" name="password" id="password" placeholder="password here">
+  </div>
+  
+  <button type="submit" name="submit" class="btn btn-primary">Login</button>
+</form>
+  </div>
+</div>
+</fieldset>
+ 
 
-
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
 </html>
